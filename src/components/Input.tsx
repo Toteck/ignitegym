b1809e2cc471c77ca881fc2c8a9bb57b44005386
mateus <mateusweslley14@gmail.com>
2025-01-03 14:36:04 -0300

    componente de input

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+import { ComponentProps } from "react";
+import { Input as GluestackInput, InputField } from "@gluestack-ui/themed";
+
+type Props = ComponentProps<typeof InputField>;
+
+export function Input({ ...rest }: Props) {
+  return (
+    <GluestackInput
+      bg="$gray700"
+      h="$14"
+      px="$4"
+      borderWidth="$0"
+      borderRadius="$md"
+    >
+      <InputField
+        {...rest}
+        color="$white"
+        fontFamily="$body"
+        placeholderTextColor="$gray300"
+      />
+    </GluestackInput>
+  );
+}

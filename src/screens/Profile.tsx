8ebe8577381879d15a9282fae69812584dda7535
mateus <mateusweslley14@gmail.com>
2025-01-06 16:40:03 -0300

    Inputs de Senha na tela profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,15 +1,16 @@
 import { ScrollView, TouchableOpacity } from "react-native";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack, Text, Center } from "@gluestack-ui/themed";
+import { VStack, Text, Center, Heading } from "@gluestack-ui/themed";
 
 import { Input } from "@components/Input";
 import { UserPhoto } from "@components/UserPhoto";
+import { Button } from "@components/Button";
 
 export function Profile() {
   return (
     <VStack>
       <ScreenHeader title="Perfil" />
-      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 150 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
             source={{ uri: "https:github.com/toteck.png" }}
@@ -34,6 +35,28 @@ export function Profile() {
               bg="$gray600"
               isReadOnly
             />
+          </Center>
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$12"
+            mb="$2"
+          >
+            Alterar senha
+          </Heading>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+
+            <Button title="Atualizar" />
           </Center>
         </Center>
       </ScrollView>

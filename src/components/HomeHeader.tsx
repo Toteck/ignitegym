c843d9734e767e64898aaa4672b0f2520f5ebca2
mateus <mateusweslley14@gmail.com>
2025-01-06 09:56:10 -0300

    Criação do componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { Heading, HStack, VStack, Text } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: "https://github.com/toteck.png" }}
+        alt="Imagem do usuário"
+        w="$16"
+        h="$16"
+      />
       <VStack>
         <Text color="$gray100" fontSize="$sm">
           Olá,

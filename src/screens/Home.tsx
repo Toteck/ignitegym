6c4b873a74be8df20908c335c3791cbbc948d966
mateus <mateusweslley14@gmail.com>
2025-01-06 14:43:01 -0300

    Listando exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,6 +9,16 @@ import { HomeHeader } from "@components/HomeHeader";
 import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
+  const [exercises, setExercises] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+    "Levantamento terra",
+    "2",
+    "3",
+    "4",
+    "5",
+  ]);
   const [groups, setGroups] = useState([
     "Costas",
     "Bíceps",
@@ -36,16 +46,22 @@ export function Home() {
         style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
       />
 
-      <VStack px="$8">
+      <VStack px="$8" flex={1}>
         <HStack alignItems="center" justifyContent="space-between" mb="$5">
           <Heading color="$gray200" fontSize="$md">
             Exercícios
           </Heading>
           <Text color="$gray200" fontSize="$md" fontFamily="$body">
-            4
+            {exercises.length}
           </Text>
         </HStack>
-        <ExerciseCard />
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
       </VStack>
     </VStack>
   );

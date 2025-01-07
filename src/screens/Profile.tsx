dbb937edf7b20ee20a23d8974a889caa2b5f17bd
mateus <mateusweslley14@gmail.com>
2025-01-07 10:29:26 -0300

    habilitando edição de imagem

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -9,7 +9,12 @@ import { Button } from "@components/Button";
 
 export function Profile() {
   async function handleUserPhotoSelect() {
-    await ImagePicker.launchImageLibraryAsync();
+    await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      quality: 1,
+      aspect: [4, 4],
+      allowsEditing: true,
+    });
   }
   return (
     <VStack>

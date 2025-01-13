a22e8aaa1a2c32988cecd40a1affe4a22884c252
mateus <mateusweslley14@gmail.com>
2025-01-13 11:43:50 -0300

    exibindo a foto padrão do usuário

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -3,13 +3,15 @@ import { UserPhoto } from "./UserPhoto";
 import { LogOut } from "lucide-react-native";
 import { useAuth } from "@hooks/useAuth";
 
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
+
 export function HomeHeader() {
   const { user } = useAuth();
 
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
       <UserPhoto
-        source={{ uri: "https://github.com/toteck.png" }}
+        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
         alt="Imagem do usuário"
         w="$16"
         h="$16"
